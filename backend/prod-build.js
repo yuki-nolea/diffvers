fed79e1d9bc90f60f66cd02f8fe49c2b3d8ad045
yuki-nolea <rentech080@gmail.com>
2023-07-08 16:44:38 +0900

    add express

---
+++ backend/prod-build.js
@@ -0,0 +1,18 @@
+const { build } = require('esbuild')
+const path = require('path')
+
+
+const options = {
+  entryPoints: [path.resolve(__dirname, 'src/app.ts')],
+  bundle: true,
+  target: 'es2016',
+  platform: 'node',
+  outdir: path.resolve(__dirname, '../frontend'),
+  tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+}
+
+
+build(options).catch(err => {
+  process.stderr.write(err.stderr)
+  process.exit(1)
+})
