e12d30422acc61f616b818641b30724baf445584
Debian <bitnami@ip-172-31-15-122.ap-northeast-1.compute.internal>
2023-07-31 15:45:53 +0900

    get HTML add

--- scraping/diffvers/diffvers/spiders/scrapy_zb6.py
+++ scraping/diffvers/diffvers/spiders/scrapy_zb6.py
@@ -1,17 +1,20 @@
 import scrapy
 
+from diffvers.items import DiffversItem
 
 class ScrapyZb6Spider(scrapy.Spider):
     name = "scrapy_zb6"
-    allowed_domains = ["www.zabbix.com"]
+    allowed_domains = ["zabbix.com"]
+    #allowed_domains = ['quotes.toscrape.com']
     start_urls = ["https://www.zabbix.com/documentation/6.0/en/manual/appendix/config/zabbix_server"]
+    #start_urls = ['http://quotes.toscrape.com/page/1/']
 
     def parse(self, response):
-        
+
         #レスポンスに対するパース処理
-        for post in response.css('.post-listing .post-time'):
-            yield Post(
-                url=post.css('div.post-header a::attr(href)').extract_first().strip(),
-                title=post.css('div.post-header a::text').extract_first().strip(),
-                date=post.css('div.post-header span.date a::text').extract_first().strip(),
-            )
+        #post in response.css('.post-listing .post-time')
+        #item = DiffversItem()
+        #item['url']=post.css('div.post-header a::attrs(href)').extract_first()
+        #item['title']=post.css('div.post-header a::text').extract_first()
+        #item['date']=post.css('div.post-header span.date a::text').extract_first()
+        print(response.text)
