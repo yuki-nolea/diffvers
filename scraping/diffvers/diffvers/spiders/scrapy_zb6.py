0b800d25969a97ec2c67f0041bf2fc616c6b4b29
Debian <bitnami@ip-172-31-15-122.ap-northeast-1.compute.internal>
2023-08-30 22:40:39 +0900

    add html perse code

--- scraping/diffvers/diffvers/spiders/scrapy_zb6.py
+++ scraping/diffvers/diffvers/spiders/scrapy_zb6.py
@@ -8,7 +8,7 @@ class ScrapyZb6Spider(scrapy.Spider):
     #allowed_domains = ['quotes.toscrape.com']
     start_urls = ["https://www.zabbix.com/documentation/6.0/en/manual/appendix/config/zabbix_server"]
     #start_urls = ['http://quotes.toscrape.com/page/1/']
-
+    
     def parse(self, response):
 
         #レスポンスに対するパース処理
@@ -17,4 +17,24 @@ class ScrapyZb6Spider(scrapy.Spider):
         #item['url']=post.css('div.post-header a::attrs(href)').extract_first()
         #item['title']=post.css('div.post-header a::text').extract_first()
         #item['date']=post.css('div.post-header span.date a::text').extract_first()
-        print(response.text)
+        #print(response.text)
+
+
+        result_list = []
+        div_tables = response.css('div.table-container')
+
+        for div_table in div_tables:
+            #表全てをlist化
+            table = div_table.css('td').getall()
+        
+        #
+        rtable= [(s.replace('<td>', '').replace('</td>', '')) for s in table]
+
+        #listを分割
+        def split_list(rtable, n):
+            for idx in range(0, len(rtable), n):
+                yield rtable[idx:idx + n]
+        
+        ntable = list(split_list(rtable, 5))
+
+        print(ntable)
