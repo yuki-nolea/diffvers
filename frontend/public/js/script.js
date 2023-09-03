33e1c31e9c1436a2759e14fe206281d99a755fea
yoko0306 <139221875+yoko0306@users.noreply.github.com>
2023-09-03 10:56:04 +0900

    Add files via upload

--- frontend/public/js/script.js
+++ frontend/public/js/script.js
@@ -1,101 +1,122 @@
-var rowDataArray = [
+var rowDataArray = {data:[
   {
-    process: "Zabbix server",
-    ver: "zabbix-Server_6.0",
-    param_name: "MySQL/Percona",
-    mandatory: "どれか一つ",
-    val_range: "8.0.X",
-    val_default: "なし",
-    param_desc: "MySQL (または Percona) を Zabbix バックエンド データベースとして使用する場合に必要です。 InnoDB エンジンが必要です。 サーバー/プロキシの構築には MariaDB Connector/C ライブラリを使用することをお勧めします。"
+    process: {value: "Zabbix server",diff: false},
+    ver: {value:"6.0",diff:false},
+    param_name: {value:"MySQL/Percona",diff:false},
+    mandatory: {value:"どれか一つ",diff:false},
+    val_range: {value:"8.0.X",diff:false},
+    val_default: {value:"なし",diff:false},
+    param_desc: {value:"MySQL (または Percona) を Zabbix バックエンド データベースとして使用する場合に必要です。 InnoDB エンジンが必要です。 サーバー/プロキシの構築には MariaDB Connector/C ライブラリを使用することをお勧めします。",diff:false}
   },
   {
-      version: "zabbix-Server_5.0",
-      name: "MariaDB",
-      required: "どれか一つ",
-      range: "10.5.00-10.8.X",
-      defaultValue: "なし",
-      description: "InnoDB エンジンが必要です。 サーバー/プロキシの構築には MariaDB Connector/C ライブラリを使用することをお勧めします。"
-  }
-];
+    process: {value: "Zabbix server",diff: false},
+    ver: {value:"5.0",diff:false},
+    param_name: {value:"MySQL/Percona",diff:false},
+    mandatory: {value:"あああ",diff:false},
+    val_range: {value:"8.0.X",diff:false},
+    val_default: {value:"なし",diff:false},
+    param_desc: {value:"ああああMySQL (または Percona) を Zabbix バックエンド データベースとして使用する場合に必要です。 InnoDB エンジンが必要です。 サーバー/プロキシの構築には MariaDB Connector/C ライブラリを使用することをお勧めします。",diff:false}  }
+]};
 
+//HTMLのid=versionを取得
 const verSelectBox1 = document.getElementById("version_1");
 const verSelectBox2 = document.getElementById("version_2");
 
-
+/*
 function showData() {
+  //HTMLのid=versionを取得
   var version1Select = document.getElementById("version_1");
   var version2Select = document.getElementById("version_2");
+  //HTMLのversionのvalueを取得
   var selectedValue1 = version1Select.value;
   var selectedValue2 = version2Select.value;
-
+　//テーブル(形式)を取得
   var table1 = document.getElementById("tbl_1_body");
   var table2 = document.getElementById("tbl_2_body");
-
+//テーブルの初期化
   table1.innerHTML = "";
   table2.innerHTML = "";
 
     for (var i = 0; i < rowDataArray.length; i++) {
-      var rowData = rowDataArray[i];
-
-      if (selectedValue1 === rowData.version) {
+      var rowData = rowDataArray.data[i];
+      console.log(rowData.ver.value);
+//htmlのバージョンとrowData.versionが一致していたらテーブルを作成
+      if (selectedValue1 === rowData.ver.value) {
           var row1 = createTableRow(rowData);
           table1.appendChild(row1);
       }
 
-      if (selectedValue2 === rowData.version) {
+      if (selectedValue2 === rowData.ver.value) {
           var row2 = createTableRow(rowData);
           table2.appendChild(row2);
       }
   }
 }
+*/
 
-function createTableRow(rowData) {
+//テーブルに行追加
+/*
+  function createTableRow(rowData) {
   var row = document.createElement("tr");
 
   var nameCell = document.createElement("td");
-  nameCell.textContent = rowData.name;
+  nameCell.textContent = rowData.param_name.value;
   row.appendChild(nameCell);
 
   var requiredCell = document.createElement("td");
-  requiredCell.textContent = rowData.required;
+  requiredCell.textContent = rowData.mandatory.value;
   row.appendChild(requiredCell);
 
   var rangeCell = document.createElement("td");
-  rangeCell.textContent = rowData.range;
+  rangeCell.textContent = rowData.val_range.value;
   row.appendChild(rangeCell);
 
   var defaultValueCell = document.createElement("td");
-  defaultValueCell.textContent = rowData.defaultValue;
+  defaultValueCell.textContent = rowData.val_default.value;
   row.appendChild(defaultValueCell);
 
   var descriptionCell = document.createElement("td");
-  descriptionCell.textContent = rowData.description;
+  descriptionCell.textContent = rowData.param_desc.value;
   row.appendChild(descriptionCell);
 
   return row;
 }
+*/
 
+//テーブル表示
 const showTable = async (table) => {
+  //HTMLのid=versionのvalueを取得
   const selectedVersion1 = verSelectBox1.value;
   const selectedVersion2 = verSelectBox2.value;
-	
+	//HTMLのテーブルのbody(tbody)を取得
   const table1 = document.getElementById("tbl_1_body");
   const table2 = document.getElementById("tbl_2_body");
-  
+  //HTMLにて非表示で作成
   const rowTemplate1 = table1.querySelector(".zbx_row_template");
   const rowTemplate2 = table2.querySelector(".zbx_row_template");
-  
+  //テーブル内データを初期化
   table1.innerHTML = "";
   table2.innerHTML = "";
 
-  const rowDataArray = await axios.get("http://hirasyain.link:3000/zbx/all");
+  //const rowDataArray = await axios.get("http://hirasyain.link:3000/zbx/all");
   console.log(rowDataArray);
 
+//itemに違った場合にdiff=trueになる
+
+  for(const item1 of rowDataArray.data){
+    for(const item2 of rowDataArray.data) {
+       if(item1.param_name.value == item2.param_name.value){
+        if(item1.param_desc.value != item2.param_desc.value){
+          item1.param_desc.diff = item2.param_desc.diff = true;
+        }
+       }
+    }
+  }
+  console.log(rowDataArray.data);
 
   let rowsIndex1 = 1;
-
   for(const item of rowDataArray.data) {
-    if(item.ver === selectedVersion1) {
+    if(item.ver.value === selectedVersion1) {
       const row = createRow(item, rowTemplate1, rowsIndex1++);
       table1.appendChild(row);
     }
@@ -104,7 +125,7 @@ const showTable = async (table) => {
 
   let rowsIndex2 = 1;
   for(const item of rowDataArray.data) {
-    if(item.ver === selectedVersion2) {
+    if(item.ver.value === selectedVersion2) {
       const row = createRow(item, rowTemplate2, rowsIndex2++);
       table2.appendChild(row);
     }
@@ -113,32 +134,28 @@ const showTable = async (table) => {
 
 
 const createRow = (rowData, template, index) => {
-
-  //template no copi- wo sakusei suru
+  // テンプレートのコピーを作成
   const row = template.cloneNode(true);
   row.hidden = false;
 
-  console.log("row = ", row)
+  console.log("row = ", row);
 
-  // rowData no atai wo k to v ni dainyu si ru-pu suru
-  for(const k in rowData) {
+  // rowDataの各プロパティにアクセス
+  for (const key in rowData) {
+    if (key === 'id' || key === 'ver') continue;
 
-    if(k == 'id' || k == 'ver') continue;
+    const id = '.' + key + '_';
 
-    const id = '.' + k + '_';
+    console.log(id);
 
-	  console.log(id);
-
-    //k ha parame-ta no namae katu hyou no seru no id to itti site iru node seru shutoku dekiru
-    const ele = row.querySelector(id + '0');
-
-    //console.log(ele)
-
-    // shutoku sita seru no atai wo v nisuru
-    ele.innerHTML = rowData[k];
-    ele.id = id + index;
+    // プロパティのデータ型がオブジェクトで、その中に 'value' プロパティがあるかをチェック
+    if (typeof rowData[key] === 'object' && 'value' in rowData[key]) {
+      const value = rowData[key].value;
+      const ele = row.querySelector(id + '0');
+      ele.innerHTML = value;
+      ele.id = id + index;
+    }
   }
-
   console.log(row);
 
   return row;
@@ -148,4 +165,3 @@ showTable();
 
 verSelectBox1.addEventListener('change', showTable);
 verSelectBox2.addEventListener('change', showTable);
-
