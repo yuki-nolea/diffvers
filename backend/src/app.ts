f7c92c6350e0c6699564d88ddeda10115162bb82
yuki-nolea <rentech080@gmail.com>
2024-01-26 22:58:06 +0900

    support https

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import path from 'path'
+import https from 'https'
+import fs from 'fs'
 
 import zbxRouter from '@/routers/zbx'
 import {sys_logger, err_logger, con_logger} from '@/model/logger'
@@ -23,6 +25,13 @@ app.get('/test', (req: any, res: any) => {
   res.json({id: 1, name: "aaaaa"});
 })
 
+const server = https.createServer(
+  {
+    key: fs.readFileSync('/opt/bitnami/letsencrypt/certificates/hirasyain.link.key'),
+    cert: fs.readFileSync('/opt/bitnami/letsencrypt/certificates/hirasyain.link.crt'),
+  },
+  app
+);
 
 export default app.listen(port, () => {
   con_logger.info(`App is running at http://localhost:${port}`)
